5c7987d424a9a739d6f4191060346c020a4c0706
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-08-06 21:26:38 +0530

    intial commit

---
+++ AssignLib/DataValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignLib
+{
+   
+    public class DataValidator
+    {
+        public bool IsValidEmail(string email)
+        {     // Your email validation logic here (simplified for example)
+            return email.Contains("@") && email.Contains(".");
+        }
+    }
+
+}
